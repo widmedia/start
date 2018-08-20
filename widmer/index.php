aabb2ba2f506403c0a6b926dc0f4bc6f0196accc
Daniel Widmer <widmer@web-organizer.ch>
2018-08-20 21:37:06 +0200

    just formatting

--- widmer/index.php
+++ widmer/index.php
@@ -40,8 +40,7 @@
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <div class="section categories">
     <div class="container">
-	    <?php
-      
+     <?php
       //                  ...those vary              ...  ... those are equal for all 3 cases ...
       function printLinks($modulo, $divClass, $sqlString, $dbConnection, $href, $endHref, $endDiv) {
         if ($result = $dbConnection->query($sqlString)) {
@@ -57,28 +56,26 @@
           $result->close(); // free result set
         } // if  
       } // function  
-        
-		// get the data out from the data base
-		$sqlStringCat1 = "SELECT * FROM `links` WHERE userid = 1 AND category = 1 ORDER BY `links`.`sort` ASC LIMIT 1000"; // <ToDo> change userid to be variable 
-    $sqlStringCat2 = "SELECT * FROM `links` WHERE userid = 1 AND category = 2 ORDER BY `links`.`sort` ASC LIMIT 1000"; 
-    $sqlStringCat3 = "SELECT * FROM `links` WHERE userid = 1 AND category = 3 ORDER BY `links`.`sort` ASC LIMIT 1000"; 
-        
-		$divClass4Columns = "<div class=\"four columns category linktext\">";
-    $divClass3Columns = "<div class=\"three columns linktext\">";
-    $href             = "<a href=\"";
-		$endHref          =  "\" target=\"_blank\" class=\"button button-primary\">";
-    $endDiv           = "</a></div>";
-    
-    
-    echo "<h3 class=\"section-heading\">News</h3><div class=\"row\">";   
-    printLinks(3, $divClass4Columns, $sqlStringCat1, $dbConnection,$href,$endHref,$endDiv);        
-    
-    echo "</div><h3 class=\"section-heading\">Work</h3><div class=\"row\">";
-    printLinks(4, $divClass3Columns, $sqlStringCat2, $dbConnection,$href,$endHref,$endDiv);      
-        
-    echo "</div><h3 class=\"section-heading\">Div</h3><div class=\"row\">";
-    printLinks(3, $divClass4Columns, $sqlStringCat3, $dbConnection,$href,$endHref,$endDiv);    
-   	?>                
+      // get the data out from the data base
+      $sqlStringCat1 = "SELECT * FROM `links` WHERE userid = 1 AND category = 1 ORDER BY `links`.`sort` ASC LIMIT 1000"; // <ToDo> change userid to be variable 
+      $sqlStringCat2 = "SELECT * FROM `links` WHERE userid = 1 AND category = 2 ORDER BY `links`.`sort` ASC LIMIT 1000";
+      $sqlStringCat3 = "SELECT * FROM `links` WHERE userid = 1 AND category = 3 ORDER BY `links`.`sort` ASC LIMIT 1000";
+      
+      $divClass4Columns = "<div class=\"four columns category linktext\">";
+      $divClass3Columns = "<div class=\"three columns linktext\">";
+      $href             = "<a href=\"";
+      $endHref          =  "\" target=\"_blank\" class=\"button button-primary\">";
+      $endDiv           = "</a></div>";
+      
+      echo "<h3 class=\"section-heading\">News</h3><div class=\"row\">";
+      printLinks(3, $divClass4Columns, $sqlStringCat1, $dbConnection,$href,$endHref,$endDiv);
+      
+      echo "</div><h3 class=\"section-heading\">Work</h3><div class=\"row\">";
+      printLinks(4, $divClass3Columns, $sqlStringCat2, $dbConnection,$href,$endHref,$endDiv);
+      
+      echo "</div><h3 class=\"section-heading\">Div</h3><div class=\"row\">";
+      printLinks(3, $divClass4Columns, $sqlStringCat3, $dbConnection,$href,$endHref,$endDiv);
+    ?>                
       </div>
     </div> <!-- /container -->
     
@@ -94,8 +91,6 @@
       </div>
     </div>
   </div> <!-- /section categories -->
-
-
 
 <!-- End Document -->
 </body>
