5c1c1e4bb6beb3fca543e01085e1b7807fa0fba0
Daniel Widmer <daniel.widmer@varian.com>
2018-08-27 12:48:08 +0200

    change on errorpage

--- widmer/link.php
+++ widmer/link.php
@@ -6,8 +6,8 @@
       die("Connection failed: " . $dbConnection->connect_error);
   }
 
-  $errorString = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"><title>Errorpage</title></head>
-  <body>Something related to the data base went wrong... well, that doesn't help that much, does it?  But you might still want to inform me (sali@widmedia.ch) or just try again later</body></html>";
+  $errorString = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"><title>Errorpage</title><link rel=\"stylesheet\" href=\"css/skeleton.css\" type=\"text/css\"></head>
+  <body><br />Something related to the data base went wrong (in file link.php)... well, that doesn't help that much, does it?<br />But you might still want to inform me (sali@widmedia.ch) or just try again later</body></html>";
 
   // id as post / get?
   $idFromGet = htmlspecialchars($_GET["id"]);
@@ -16,18 +16,20 @@
     $idFiltered = $idFromGet;
 
     // important to verify the userid as well
-    $sqlSelectString = "SELECT link FROM `links` WHERE userid = 1 AND id = ".$idFiltered." LIMIT 1";
+    $sqlSelectString = "SELECT link FROM `links` WHERE userid = 1 AND id = ".$idFiltered." LIMIT 2"; // should always return just one
     $sqlUpdateString = "UPDATE `links` SET cntTot = cntTot + 1 WHERE userid = 1 AND id = ".$idFiltered;
 
     if ($result = $dbConnection->query($sqlSelectString)) {
-      $row = $result->fetch_assoc();
+      if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
 
-      if ($dbConnection->query($sqlUpdateString)) {
-        // everything went as expected, no errors
-        $result->close(); // free result set
-        header("Location: ".$row["link"]);
-        exit();
-      } 
+        if ($dbConnection->query($sqlUpdateString)) {
+          // everything went as expected, no errors
+          $result->close(); // free result set
+          header("Location: ".$row["link"]);
+          exit();
+        } 
+      }
     } 
   } 
    
