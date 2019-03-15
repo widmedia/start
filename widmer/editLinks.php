b5e27ab0bb883075cc53c0609afe28dceaa581bb
Daniel Widmer <daniel.widmer@varian.com>
2019-03-15 16:15:42 +0100

    wip on editLinks.php

--- widmer/editLinks.php
+++ widmer/editLinks.php
@@ -18,6 +18,7 @@
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/skeleton.css">
   <link rel="stylesheet" href="css/custom.css">
+  <!-- some site specific code (might be moved to custom.css later) -->
   <style>
     button.link {
       background:none;
@@ -29,7 +30,7 @@
       border-bottom:1px solid #444; 
       cursor: pointer;
     }
-</style>
+  </style>
   
   <!-- Favicon -->
   <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
@@ -42,94 +43,114 @@
     <div class="container">
      <?php     
       require_once("php/dbConnection.php"); // this will return the $dbConnection variable
-
       // Check connection
       if ($dbConnection->connect_error) { die("Connection failed: " . $dbConnection->connect_error); }      
-
-      $category = 2; // TODO: category is fixed...
+      
       $userid = 1;   // TODO: userid is fixed...
+      
+      // TODO the numbering of the 'action' is not logical (2 comes before 1). Could also work with enum/list
+      
       
       // Form processing
       $actionFromPost = htmlspecialchars($_POST["action"]); // this should be either an integer or not set.
-      $actionFiltered = 0;
+      $categoryFromPost = htmlspecialchars($_POST["categoryInput"]); // this should be an integer (when action is set)
+      $actionSafe = 0;
+      $categorySafe = 0;
       $dispErrorMsg = 0;      
+      $heading = "News"; // TODO: section headings are 3 fixed values. Need another table for that
+      
       if (filter_var($actionFromPost, FILTER_VALIDATE_INT)) {
-        $actionFiltered = $actionFromPost;
-        if($actionFiltered == 1) {  // have a valid action (is a post variable)
+        $actionSafe = $actionFromPost;        
+        
+        if (filter_var($categoryFromPost, FILTER_VALIDATE_INT)) {
+          $categorySafe = $categoryFromPost;
+          if ($categoryFromPost == 2) { $heading = "Work"; } elseif ($categoryFromPost == 3) { $heading = "Div"; }           
+        } else { $dispErrorMsg = 5; }// have an integer on category
+        // TODO: if-else-switch monster construct is kind of, well, a monster... and not really correct.
+       
+        switch ($actionSafe) {
+        case 2: // category selection thing        
+            // I need fields to 
+            // done: a) add a new link with "link name" / "link href" 
+            // b) edit/delete existing links: edit "link name" / "link href". Delete the whole link
+            // c) Change link ordering: add +/- buttons  --> this submits instantly. Needs to swap values (is more robust than +/- 1, works with gaps as well and basically same operation)           
+
+            // b/c: TODO: this currently just prints the present state
+            require_once("functions.php");
+            
+            $divClass3Columns = "<div class=\"three columns linktext\">";  // using the smallest size (for four links in one row)
+            $sqlString = "SELECT * FROM `links` WHERE userid = ".$userid." AND category = ".$categorySafe." ORDER BY `links`.`sort` ASC LIMIT 100"; // more than 100 links do not make sense
+            
+            echo "<h3 class=\"section-heading\">".$heading."</h3><div class=\"row\">";
+            printLinks(4, $divClass3Columns, $sqlString, $dbConnection); // this function is defined in the functions.php file
+            echo "</div>";
+            
+            // this implements a) add a new link. TODO: could also serve as edit link? 
+            echo "<form action=\"editLinks.php\" method=\"post\">
+                    <div class=\"row\"><div class=\"twelve columns\"><h3 class=\"section-heading\">New link</h3><input name=\"action\" type=\"hidden\" value=\"1\"><input name=\"categoryInput\" type=\"hidden\" value=\"".$categorySafe."\"></div></div>
+                    <div class=\"row\">
+                      <div class=\"four columns\"><input name=\"link\" type=\"url\"  maxlength=\"1023\" value=\"https://\" required></div>
+                      <div class=\"four columns\"><input name=\"text\" type=\"text\" maxlength=\"255\"  value=\"text\" required></div>
+                      <div class=\"four columns\"><input name=\"submit\" type=\"submit\" value=\"Add link\"></div>
+                    </div>
+                  </form>";          
+          break;  
+        case 1: // have a valid action. 1 = add a link 
           if (filter_var($_POST["link"], FILTER_VALIDATE_URL, FILTER_FLAG_SCHEME_REQUIRED)) { // have a validUrl. require the http(s)://-part as well. 
             // filtering it for both HTML display and sqli insertion
             $textSafe = htmlspecialchars(mysqli_real_escape_string($dbConnection, $_POST["text"]));                               
             $linkSafe = htmlspecialchars(mysqli_real_escape_string($dbConnection, $_POST["link"]));
-            
+          
             // NB: statement below is not allowed as the same table is used for insert and for data generation. Need to split into two operations...
             // $sql = "INSERT INTO `links` (`id`, `userid`, `category`, `sort`, `text`, `link`, `cntTot`) VALUES 
             // (NULL, '1', '2', ((SELECT MAX(sort) FROM `links` WHERE `userid` = 1 AND `category` = 2) + 1), '".$textSafe"', '".$linkSafe."', '0')";
-            $sqlGetMax = "SELECT MAX(sort) FROM `links` WHERE `userid` = ".$userid." AND `category` = ".$category;
+            $sqlGetMax = "SELECT MAX(sort) FROM `links` WHERE `userid` = ".$userid." AND `category` = ".$categorySafe;
             
             if ($result = $dbConnection->query($sqlGetMax)) {
               $row = $result->fetch_row(); // guaranteed to get only one row and one column
               $maxPlus1 = ($row[0]) + 1;              
                           
-              $sqlInsert = "INSERT INTO `links` (`id`, `userid`, `category`, `sort`, `text`, `link`, `cntTot`) VALUES (NULL, '".$userid."', '".$category."', '".$maxPlus1."', '".$textSafe."', '".$linkSafe."', '0')";
+              $sqlInsert = "INSERT INTO `links` (`id`, `userid`, `category`, `sort`, `text`, `link`, `cntTot`) VALUES (NULL, '".$userid."', '".$categorySafe."', '".$maxPlus1."', '".$textSafe."', '".$linkSafe."', '0')";
               if ($result = $dbConnection->query($sqlInsert)) {
                 echo "<h3 class=\"section-heading\">Link added</h3><div class=\"row\">\n";
                 echo "<div class=\"three columns linktext\"><a href=\"".$linkSafe."\" target=\"_blank\" class=\"button button-primary\">".$textSafe."</a><span class=\"counter\">0</span></div>\n";
                 echo "<div class=\"nine columns linktext\">&nbsp</div>\n";
                 echo "</div>";                   
-              } else { $dispErrorMsg = 4;} // insert query did work
-            } else {$dispErrorMsg = 3;} // getMax query did work
-          } else { $dispErrorMsg = 2;} // have a validUrl -> TODO: add an additional error msg here because this really depends on user input
-        } else { $dispErrorMsg = 1;} // have a valid action, would like to add a link  
-        
+              } else { $dispErrorMsg = 4; } // insert query did work
+            } else { $dispErrorMsg = 3; } // getMax query did work
+          } else { $dispErrorMsg = 2; } // have a validUrl -> TODO: add an additional error msg here because this really depends on user input
+          break;
+        default: 
+          $dispErrorMsg = 1;
+        } // switch
         if ($dispErrorMsg > 0) {
           echo "<h3 class=\"section-heading\">Error</h3><div class=\"row\">";
           echo "<div class=\"nine columns linktext\">'Something' at step ".$dispErrorMsg." went wrong when processing user input data (very helpful error message, I know...). Might try again?</div>\n";
           echo "<div class=\"three columns linktext\">&nbsp</div>\n</div>";                   
           exit(); // finish the php part
-        }
-      } else { // form processing: have an integer. When entering the page, there is no $actionFromPost set... It's not a fault
+        } // dispErrorMsg > 0        
+      } else { // form processing: do not have a valid integer. When entering the page, there is no $actionFromPost set... Most probably it's not a fault but just the entry point
         echo "<h2 class=\"section-heading\">What would you like to edit?</h2><div class=\"row\">";
+
+        // TODO: the text 'News' has to be retrieved from data base      
+        echo "<div class=\"four columns\"><form action=\"editLinks.php\" method=\"post\">
+        <input name=\"action\" type=\"hidden\" value=\"2\"><input name=\"categoryInput\" type=\"hidden\" value=\"1\">
+        <input name=\"submit\" type=\"submit\" value=\"Category News\"></form></div>";        
+        echo "<div class=\"four columns\"><form action=\"editLinks.php\" method=\"post\">
+        <input name=\"action\" type=\"hidden\" value=\"2\"><input name=\"categoryInput\" type=\"hidden\" value=\"2\">
+        <input name=\"submit\" type=\"submit\" value=\"Category Work\"></form></div>";
+        echo "<div class=\"four columns\"><form action=\"editLinks.php\" method=\"post\">
+        <input name=\"action\" type=\"hidden\" value=\"2\"><input name=\"categoryInput\" type=\"hidden\" value=\"3\">
+        <input name=\"submit\" type=\"submit\" value=\"Category Div\"></form></div>";
+        echo "</div>\n"; // row
+
+        // --- working here --- //    
         
-        // --- working here --- //     
-        echo "<div class=\"four columns\"><form action=\"editLinks.php\" method=\"post\"><h3 class=\"section-heading\"><input name=\"action\" type=\"hidden\" value=\"2\"><button class=\"link\" name=\"submit\" type=\"submit\" value=\"Category News\"></h3></form></div>";
-        echo "<div class=\"four columns\"><h3 class=\"section-heading\">Category Work</h3></div>";
-        echo "<div class=\"four columns\"><h3 class=\"section-heading\">Category Div</h3></div></div>";
-      
-        echo "<div class=\"row\"><div class=\"twelve columns\"><hr /></div></div>";
-        echo "<div class=\"row\"><div class=\"six columns\"><a href=\"#\">set all counters to 0</a></div><div class=\"six columns\"><a href=\"#\">(account management)</a></div></div>";
-      }
-
-      require_once("functions.php");
-      
-      $sqlString = "SELECT * FROM `links` WHERE userid = ".$userid." AND category = ".$category." ORDER BY `links`.`sort` ASC LIMIT 100"; // more than 100 links do not make sense
-
-      $divClass3Columns = "<div class=\"three columns linktext\">";
-      $href             = "<a href=\"";
-      $endHref          =  "\" target=\"_blank\" class=\"button button-primary\">";
-      $endDiv           = "</a></div>";
-      
-      echo "<h3 class=\"section-heading\">Work</h3><div class=\"row\">";
-      printLinks(4, $divClass3Columns, $sqlString, $dbConnection); // this function is defined in the functions.php file
-      echo "</div>";
-      
-      
-      // code above prints the current state. Now I need fields to 
-      // a) add a new link with "link name" / "link href" 
-      // b) edit/delete existing links: edit "link name" / "link href". Delete the whole link
-      // c) Change link ordering: add +/- buttons  --> this submits instantly. Needs to swap values (is more robust than +/- 1, works with gaps as well and basically same operation)
+        echo "<div class=\"row\"><div class=\"twelve columns\"><hr /></div></div>\n";
+        echo "<div class=\"row\"><div class=\"six columns\"><a href=\"#\">set all counters to 0</a></div><div class=\"six columns\"><a href=\"#\">(account management)</a></div></div>\n";        
+      }// action = integer           
     ?>                
-    </div> <!-- /container -->
-    <div class="container">
-      <form action="editLinks.php" method="post">
-      <h3 class="section-heading">New link<input name="action" type="hidden" value="1"></h3>
-      <div class="row">          
-          <div class="four columns"><input name="link" type="url"  maxlength="1023" value="https://" required></div>
-          <div class="four columns"><input name="text" type="text" maxlength="255"  value="text" required></div>
-          <div class="four columns"><input name="submit" type="submit" value="Add link"></div>
-      </div>
-      </form>
-    </div>
-    
+    </div> <!-- /container -->    
     <!-- ToDo: code below should be included. Somehow...  -->
     <div class="section noBottom">
       <div class="container">
--- widmer/link.php
+++ widmer/link.php
@@ -1,4 +1,5 @@
  <?php
+  // this file does the redirection and increases the link counter by one
   require_once("php/dbConnection.php"); // this will return the $dbConnection variable
 
   // Check connection
@@ -6,15 +7,14 @@
       die("Connection failed: " . $dbConnection->connect_error);
   }
 
-  $errorString = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"><title>Errorpage</title><link rel=\"stylesheet\" href=\"css/skeleton.css\" type=\"text/css\"></head>
-  <body><br />Something related to the data base went wrong (in file link.php)... well, that doesn't help that much, does it?<br />But you might still want to inform me (sali@widmedia.ch) or just try again later</body></html>";
-
-  // id as post / get?
+  // id as 'get' parameter
   $idFromGet = htmlspecialchars($_GET["id"]);
   $idFiltered = 0;
   if (filter_var($idFromGet, FILTER_VALIDATE_INT)) {
     $idFiltered = $idFromGet;
 
+    // TODO: add another sqlspecialchars or similar. Just to make sure
+    
     // important to verify the userid as well
     $sqlSelectString = "SELECT link FROM `links` WHERE userid = 1 AND id = ".$idFiltered." LIMIT 2"; // should always return just one
     $sqlUpdateString = "UPDATE `links` SET cntTot = cntTot + 1 WHERE userid = 1 AND id = ".$idFiltered;
@@ -34,5 +34,7 @@
   } 
    
   // should never reach this code...
-  echo $errorString; 	
+  echo "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"><title>Errorpage</title><link rel=\"stylesheet\" href=\"css/skeleton.css\" type=\"text/css\"></head>
+  <body><br />Something related to the data base went wrong (in file link.php)... well, that doesn't help that much, does it?<br />But you might still want to inform me (sali@widmedia.ch) or just try again later</body></html>";
+
 ?>
