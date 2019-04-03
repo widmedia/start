9b93f600200c854cf6eb615d31a4d810c0acff4c
Daniel Widmer <daniel.widmer@varian.com>
2019-04-03 16:29:58 +0200

    added userid, same validate/sanitize as in other files

--- widmer/link.php
+++ widmer/link.php
@@ -1,36 +1,35 @@
  <?php
   // this file does the redirection and increases the link counter by one
-  require_once('php/dbConnection.php'); // this will return the $dbConnection variable as 'new mysqli'
-  if ($dbConnection->connect_error) { die('Connection failed: ' . $dbConnection->connect_error); }
-
+  require_once('functions.php');
+  $dbConnection = initialize();  // does the session start and opens connection to the data base. Returns the dbConnection variable
+  
   // id as 'get' parameter
-  $idFromGet = htmlspecialchars($_GET['id']);
-  $idFiltered = 0;
-  if (filter_var($idFromGet, FILTER_VALIDATE_INT)) {
-    $idFiltered = $idFromGet;
-
-    // TODO: add another sqlspecialchars or similar. Just to make sure
+  $idUnsafe = filter_var(substr($_GET['id'], 0, 11), FILTER_SANITIZE_NUMBER_INT); // this is an integer (max 11 characters)
+  $idSafe = 0;
+  
+  
+  if (filter_var($idUnsafe, FILTER_VALIDATE_INT)) { 
+    $idSafe = $idUnsafe; // because it's an integer, it is safe for both sql and html§
+    $userid = getUserid();
     
     // important to verify the userid as well
-    $sqlSelectString = 'SELECT link FROM `links` WHERE userid = 1 AND id = '.$idFiltered.' LIMIT 2'; // should always return just one
-    $sqlUpdateString = 'UPDATE `links` SET cntTot = cntTot + 1 WHERE userid = 1 AND id = '.$idFiltered;
+    $sqlSelectString = 'SELECT link FROM `links` WHERE userid = '.$userid.' AND id = '.$idSafe; // should always return just one
+    $sqlUpdateString = 'UPDATE `links` SET cntTot = cntTot + 1 WHERE userid = '.$userid.' AND id = '.$idSafe;
 
     if ($result = $dbConnection->query($sqlSelectString)) {
       if ($result->num_rows == 1) {
-        $row = $result->fetch_assoc();
-
+        $row = $result->fetch_row();
         if ($dbConnection->query($sqlUpdateString)) {
           // everything went as expected, no errors
           $result->close(); // free result set
-          header('Location: '.$row['link']);
+          header('Location: '.$row[0]);
           exit();
-        } 
-      }
-    } 
-  } 
+        } // sql update      
+      } // one row result
+    } // sql select
+  } // valid integer
    
   // should never reach this code...
-  echo '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Errorpage</title><link rel="stylesheet" href="css/skeleton.css" type="text/css"></head>
-  <body><br />Something related to the data base went wrong (in file link.php)... well, that doesn\'t help that much, does it?<br />But you might still want to inform me (sali@widmedia.ch) or just try again later</body></html>';
-
+  die('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Errorpage</title><link rel="stylesheet" href="css/skeleton.css" type="text/css"></head>
+  <body><br>Something related to the data base went wrong (in file link.php)... well, that doesn\'t help that much, does it?<br>But you might still want to inform me (sali@widmedia.ch) or just try again later</body></html>');
 ?>
