5ed41b2fa6fe1064572ca574af431eae04e60994
Daniel Widmer <daniel.widmer@varian.com>
2019-06-04 17:58:16 +0200

    some work on adding a new user

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
   // a: unsecured
   // a1: first visit, direct visit (people typing widmedia.ch/start)
   // a2: log out function. do=1. linked from within the secure site, doing the logout
-  // a3: add new user function. do=2. linked from the insecure site
+  // a3: add new user function. do=2. linked from the insecure site. TODO
   // b: secured
-  // b1: link with userid and key: do=3&userid=7&key=ABCDEF1234...
+  // b1: link with userid and key: userid=7&key=ABCDEF1234...
   // b2: direct visit (do=0), cookie is set
   // b3: login form done, do=4, (email/pw as POST). setCookie is checked or not
   
@@ -98,6 +98,31 @@
     }    
   }
   
+  // TODO: this might be merged with a similar function (printUserEdit) in editUser.php
+  function printNewUserForm() {
+    // TODO: design not yet very nice
+    echo '<h3 class="section-heading">New account</h3>
+    <form action="index.php?do=3" method="post">
+    <div class="row"><div class="two columns">email: </div><div class="ten columns"><input name="email" type="email" maxlength="127" value="" required size="20"></div></div>
+    <div class="row"><div class="two columns"><input type="checkbox" name="hasPw" value="1" checked onclick="pwFieldToggle();"></div><div class="ten columns" style="text-align: left;">password protection for this account</div></div>
+    <div class="row"><div class="two columns">password: </div><div class="ten columns"><input id="passwordTextField" name="password" type="password" maxlength="63" value="" size="20"></div></div>
+    <div class="row"><div class="two columns"><input type="checkbox" name="hasKey" value="1" onclick="magicKeyToggle();"></div><div class="ten columns" style="text-align: left;">"magic key" login for easier access</div></div>
+    <div class="row"><div class="twelve columns">login like: <span id="magicKeyExample" style="text-decoration: line-through;">https://widmedia.ch/start/index.php?userid=2&key=9233B77ADC7F47F5A7F2EC5F</span></div></div>    
+    <div class="row"><div class="twelve columns"><input name="create" type="submit" value="create account"></div></div>
+    </form>
+    ';   
+  } // function
+  
+  function printTitle() {
+    // TODO: need to advertise the /start functionality. With some kind of animation...
+    echo '<h2 class="section-heading">widmedia.ch/start</h2>
+    <div class="row" style="font-weight: bold; font-size: larger; text-align: left"><div class="twelve columns">a simple customizable start page, a personal link collection</div></div>
+    <div class="row"><div class="twelve columns"><p>&nbsp;</p></div></div>
+    <div class="row"><div class="twelve columns">Try it out: <a href="index.php?userid=2&key=9233B77ADC7F47F5A7F2EC5F" class="button button-primary">log in as the test user</a></div>
+    </div>';
+    printHr();    
+  } // function
+  
   function printEntryPoint($dbConnection) {
     // TODO: this will change...    
     
@@ -105,29 +130,23 @@
     // I have a table `login`: id / userid / hasPw / hasKey / pwHash / magicKey. Use it for login-related stuff
     // --> TODO: new user requires items there as well, delete an existing user has to remove it once more
     // selection between
-    // a: login as test user, try stuff (test user does have a pw and a key)
-    // b: open a new account --> more or less the editUser page
+    // a: login as test user, try stuff (test user does have both a pw and a key)
+    // b: TODO: prio_1: open a new account --> more or less the editUser page
     // c: login as existing --> more or less the page below
     
-    // need to advertise the /start functionality. With some kind of animation...
-    echo '<h2 class="section-heading">widmedia.ch/start</h2>
-    <div class="row" style="font-weight: bold; font-size: larger; text-align: left"><div class="twelve columns">a simple customizable start page, a personal link collection</div></div>
-    <div class="row"><div class="twelve columns"><p>&nbsp;</p></div></div>
-    <div class="row"><div class="twelve columns">Try it out: <a href="index.php?userid=2&key=9233B77ADC7F47F5A7F2EC5F" class="button button-primary">log in as the test user</a></div>
-    </div>';
-    printHr();
+    printTitle();
     
     echo '
     <h3 class="section-heading">Log in</h3>
     <form action="index.php?do=4" method="post">
-    <div class="row"><div class="four columns">email: </div><div class="eight columns"><input name="email" type="email" maxlength="127" value="" required size="20"></div></div>
-    <div class="row"><div class="four columns">password: </div><div class="eight columns"><input name="password" type="password" maxlength="63" value="" required size="20"></div></div>
+    <div class="row"><div class="three columns">email: </div><div class="nine columns"><input name="email" type="email" maxlength="127" value="" required size="20"></div></div>
+    <div class="row"><div class="three columns">password: </div><div class="nine columns"><input name="password" type="password" maxlength="63" value="" required size="20"></div></div>
     <div class="row" style="font-size: smaller;"><div class="twelve columns"><input type="checkbox" name="setCookie" value="1" checked>save log in information for 4 weeks</div></div>    
     <div class="row"><div class="twelve columns"><input name="login" type="submit" value="log in"></div></div>
     </form>
     <div class="row"><div class="twelve columns">&nbsp;</div></div>
     <div class="row" style="font-size: smaller;">
-      <div class="six columns"><a href="editUser.php?do=9">open a new account</a></div>
+      <div class="six columns"><a href="index.php?do=2">open a new account</a></div>
       <div class="six columns"><a href="index.php?do=9">forgot my password</a></div>
     </div>
     ';
@@ -192,6 +211,19 @@
   <!-- Favicon -->
   <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png">
+  
+  <script>
+  function pwFieldToggle() {
+    document.getElementById('passwordTextField').disabled = !(document.getElementById('passwordTextField').disabled);
+  }
+  function magicKeyToggle() {       
+    if (document.getElementById('magicKeyExample').style.textDecoration == "line-through") {
+      document.getElementById('magicKeyExample').style.textDecoration = "none";
+    } else {
+      document.getElementById('magicKeyExample').style.textDecoration = "line-through";
+    }
+  }  
+</script>
 
 </head>
 <body>
@@ -218,30 +250,30 @@
           logOut();
           break;
         case 2:
-          // TODO:
-          // form with some basic information: email.
-          // - some selection: with or without pw? ... stuff like that
-
-          // password_hash("testUserPassword", PASSWORD_DEFAULT);
-          // returns = '$2y$10$3qc.gl4eDPpXDqM7hDssquu4ThnJ9rbH7wrEkdTZd0Cg0NAjAzm.2';
-
+          printTitle();
+          printNewUserForm();
+          break;
+        case 3:
           // Adding 1 user, 3 categories, 4 links
           // add a new user but only if number of users is less than 10 (TODO: developer limitation for now)
-          if ($result = $dbConnection->query('SELECT count(*) AS `total` FROM `user`')) {
-              $row = $result->fetch_assoc();
-              $rowCnt = $row['total'];              
-              if($rowCnt < 10) {
-                $sqlInsertUser = 'INSERT INTO `user` (`id`, `email`, `lastLogin`) VALUES (NULL, "new@widmedia.ch", CURRENT_TIMESTAMP)';
-                if ($result = $dbConnection->query($sqlInsertUser)) { 
-                  $newUserid = $dbConnection->insert_id;
-                  if(newUserLinks($newUserid, $dbConnection)) {
-                    printConfirmation('Did add a new user', 'Userid: '.$newUserid.'. Login with this user: <a href="index.php?userid='.$newUserid.'">login</a>', 'six', 'six');
-                  } else { $dispErrorMsg = 24; } // links, categories insert
-                } else { $dispErrorMsg = 23; } // user insert
-              } else { $dispErrorMsg = 22; } // have less than 10
-          } else { $dispErrorMsg = 21; } // query to do the counting did not work          
-          break;
-        // TODO: case 3 is missing  
+          if (false) { // TODO: need to process the form data
+            // password_hash("testUserPassword", PASSWORD_DEFAULT);
+            // returns '$2y$10$3qc.gl4eDPpXDqM7hDssquu4ThnJ9rbH7wrEkdTZd0Cg0NAjAzm.2';
+            if ($result = $dbConnection->query('SELECT count(*) AS `total` FROM `user`')) {
+                $row = $result->fetch_assoc();
+                $rowCnt = $row['total'];              
+                if($rowCnt < 10) {
+                  $sqlInsertUser = 'INSERT INTO `user` (`id`, `email`, `lastLogin`) VALUES (NULL, "new@widmedia.ch", CURRENT_TIMESTAMP)';
+                  if ($result = $dbConnection->query($sqlInsertUser)) { 
+                    $newUserid = $dbConnection->insert_id;
+                    if(newUserLinks($newUserid, $dbConnection)) {
+                      printConfirmation('Did add a new user', 'Userid: '.$newUserid.'. Login with this user: <a href="index.php?userid='.$newUserid.'">login</a>', 'six', 'six');
+                    } else { $dispErrorMsg = 24; } // links, categories insert
+                  } else { $dispErrorMsg = 23; } // user insert
+                } else { $dispErrorMsg = 22; } // have less than 10
+            } else { $dispErrorMsg = 21; } // query to do the counting did not work          
+          }
+          break;        
         case 4:
           if (filter_var($emailUnsafe, FILTER_VALIDATE_EMAIL)) { // have a valid email
             $userid = mail2userid($emailUnsafe, $dbConnection);
