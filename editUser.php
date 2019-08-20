81aa363d5d3128bae98790ace3158c87db4bb324
Daniel Widmer <daniel.widmer@varian.com>
2019-08-20 13:39:54 +0200

    bugfix on the border of the selected style

--- editUser.php
+++ editUser.php
@@ -11,15 +11,13 @@
       $displayPwRows   = 'initial';
       $pwFieldRequired = 'required';
     }
-    
-    // later TODO: will change it to arrays as soon as I have several pictures    
-    $currentlySelectedStyle = 'border: 2px solid #faff3b;';  // to do: should use the color definition from printInlineCss. But later on, that's coming from the db anyhow
+
     $notSel = 'border: 2px dotted #000;';
-    
+    $currentlySelectedStyle = 'border: 2px solid #faff3b;';  // a bright color (not related to the designs)
+
     // 0..6 are valid selectors
     $bgBorderSel = array($notSel,$notSel,$notSel,$notSel,$notSel,$notSel,$notSel);
     $bgBorderSel[$row['styleId']] = $currentlySelectedStyle;
-        
     
     echo '
     <h3 class="section-heading"><span class="bgCol">Email / '.getLanguage($dbConn,84).'</span></h3>
@@ -45,7 +43,7 @@
     <h3 class="section-heading"><span class="bgCol">'.getLanguage($dbConn,122).'</span></h3>';
     for ($i = 0; $i < 7; $i++) {
       if (($i % 4) == 0) { echo '<div class="row">'; }
-      echo '<div class="three columns u-max-full-width"><a href="editUser.php?do=3&styleId='.$i.'" style="background-color:transparent;"><img src="images/bg/'.styleDef($i,'bgImg').'" alt="default background image" style="'.$bgBorderSel_0.' width:100%; vertical-align:middle;"></a></div>';
+      echo '<div class="three columns u-max-full-width"><a href="editUser.php?do=3&styleId='.$i.'" style="background-color:transparent;"><img src="images/bg/'.styleDef($i,'bgImg').'" alt="default background image" style="'.$bgBorderSel[$i].' width:100%; vertical-align:middle;"></a></div>';
       if (($i == 3) or ($i == 6)) { // last one does not fit into modulo function ($i % 4) == 3
         echo '</div><div class="row twelve columns">&nbsp;</div>'; 
       } 
