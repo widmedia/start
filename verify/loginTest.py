e78d9b2539a782a64d3bacb48c032160b0a9536e
Daniel Widmer <daniel.widmer@varian.com>
2019-10-16 15:52:38 +0200

    first successful login using python

--- verify/loginTest.py
+++ verify/loginTest.py
@@ -1,28 +1,33 @@
 import time
-
 from selenium import webdriver
-# from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-  
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+
+# Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
+
 driver.get("https://widmedia.ch/start/index.php")
-  
+
+# initial page title (with the login fields)
+print (driver.title)
+
+# find the elements I want to control
 email = driver.find_element_by_name("email")
 password = driver.find_element_by_name("password")
 submit = driver.find_element_by_name("login")
 
-# this account is a 'normal' account (not admin and not testUser)
 email.send_keys("widmer@web-organizer.ch")
 password.send_keys("blabla")
 
 submit.click()
-  
-wait = WebDriverWait(driver, 5)
 
-time.sleep(3) # the script itself
+try:
+    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
+    WebDriverWait(driver, 10).until(EC.title_contains("Links"))
+    
+    print(driver.title)
+    time.sleep(2) # not needed, to admire the page
 
-# TODO implement the checks (e.g. need to be on the links page, no login error, etc...)
-# print("site title is: " + driver.getTitle())
-
-# at the very end
-driver.quit()
+finally:
+    driver.quit()
