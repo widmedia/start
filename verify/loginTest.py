735bc1756994542519e79c89cd7c93d266053119
Daniel Widmer <daniel.widmer@varian.com>
2019-10-17 13:36:07 +0200

    wip: login test done

--- verify/loginTest.py
+++ verify/loginTest.py
@@ -1,36 +1,70 @@
-import time
+# import time
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-correctPassword = "blabla"
-wrongPassword = "blablabla"
+# returns a boolean
+def checkSiteTitle (driver, expectedSiteTitle):
+  try:
+    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
+    WebDriverWait(driver, 5).until(EC.title_contains(expectedSiteTitle))  # timeout in seconds    
+    print("Site title as expected: " + driver.title)
+    return True
+  except: # most probably the timeout exception. TODO: check on just the timeout exception
+    print("Site title not as expected: " + driver.title)
+    return False
+  # end try/except
+# end def
+
+def doLogin (driver, username, password):
+  # find the elements I want to control
+  emailField    = driver.find_element_by_name("email")
+  passwordField = driver.find_element_by_name("password")  
+
+  # send the login details
+  emailField.send_keys(username)
+  passwordField.send_keys(password)  
+
+  # and submit
+  passwordField.submit()  # could also use a command like: driver.find_element_by_name("login").click() 
+# end def
 
 
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
-# driver.set_window_size(800, 800)
 driver.set_window_size(500, 700) # mobile, portrait style
 
 # go to the start page
-driver.get("https://widmedia.ch/start/index.php")
+driver.get("https://widmedia.ch/start")
 
 # initial page title (with the login fields)
 print (driver.title)
 
-# find the elements I want to control
-email    = driver.find_element_by_name("email")
-password = driver.find_element_by_name("password")
-submit   = driver.find_element_by_name("login")
+# want to verify following procedure
+# 1) login with correct name/pw  -> page title is Links
+# 2) logout                      -> page title is Startpage again
+# 3) login with faulty name/pw   -> page title is still Startpage
 
-# action starts, send the login details
-email.send_keys("widmer@web-organizer.ch")
-password.send_keys(correctPassword)
-# password.send_keys(wrongPassword)
 
-# and submit
-submit.click() # could also use a command like: password.submit()
+doLogin(driver, username="widmer@web-organizer.ch", password="blabla")
+
+if (not(checkSiteTitle(driver, "Links"))):
+  print("ERROR. Login test not successful")
+  driver.quit()
+  sys.exit()
+# end if
+  
+print("OK. Login test successful") # we are now on the links page
+
+
+
+
+
+
+driver.quit() # close the browser window
+
+
 
 # element = driver.find_element_by_id("element_id")
 # element.text
@@ -38,17 +72,4 @@ submit.click() # could also use a command like: password.submit()
 # select = Select(driver.find_element_by_tag_name("select"))
 # select.deselect_all()
 # select.select_by_visible_text("Edam")
-
-
-try:
-    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
-    WebDriverWait(driver, 10).until(EC.title_contains("Links"))
-    
-    print(driver.title)
-    # time.sleep(2) # not needed, to admire the page
-
-
-# working fine, get a timeout exception when the password is not correc
-
-finally:
-    driver.quit()
+# time.sleep(2) # not needed, to admire the page
