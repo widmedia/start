e08cf1bc0cbb00410f0e3bce27cda73e43fa8a40
Daniel Widmer <daniel.widmer@varian.com>
2019-10-28 14:48:10 +0100

    replaced distinct function calls with a for loop

--- verification/run.py
+++ verification/run.py
@@ -1,67 +1,70 @@
 import sys
 from selenium import webdriver
 from functions import finish, printOkOrNot
-from loginLogout import doLoginLogout
-from newUser import doNewUser
-from editLinks import doEditLinks
+
 
 # main file to start other tests
 #------------------------------------------------------------------------------
-# available tests:
-# 1) loginLogout
-# 2) newUser
-def printUsage(allTests):
+def printUsage(ALL_TESTS):
   print("Usage: run.py [testName]")
   print("run.py: runs all the available tests")
   print("run.py testName: runs a single test")
   print("  available tests are: ", end="")
-  for test in allTests:
+  for test in ALL_TESTS:
     print(test + " ", end="")
+  print(" ")  # just a new line
 # end def
 
 
-allTests = ['loginLogout', 'newUser', 'editLinks']
+def callSingleTest(driver, testNum, ALL_TESTS, testsToRun):
+  from loginLogout import doLoginLogout
+  from newUser import doNewUser
+  from editLinks import doEditLinks
+  
+  if ALL_TESTS[testNum] in testsToRun:       
+    if testNum == 0:
+      result = doLoginLogout(driver, testNum)
+    elif testNum == 1:
+      result = doNewUser(driver, testNum)
+    elif testNum == 2: 
+      result = doEditLinks(driver, testNum)
+    else:
+      result = False
+    # end if-elif
+    if (not(result)): 
+      finish(driver)
+    print("----")
+  # end if 
+# end def
 
+
+ALL_TESTS = ['loginLogout', 'newUser', 'editLinks']
+
+
+# input processing
 testsToRun = []
 if len(sys.argv) < 2:  # this means no argument has been given. Running all tests
-  testsToRun = allTests  
+  testsToRun = ALL_TESTS  
 elif len(sys.argv) == 2:
-  if sys.argv[1] in allTests:  # find the argument
+  if sys.argv[1] in ALL_TESTS:  # find the argument
     testsToRun = [sys.argv[1]]
   else:
-    printUsage(allTests)
+    printUsage(ALL_TESTS)
   # end if 
 else:
-    printUsage(allTests)
+    printUsage(ALL_TESTS)
 # end if
 
+
+# execution of the tests
 if len(testsToRun) > 0:
   # Create a new instance of the Firefox driver
   driver = webdriver.Firefox()
   driver.set_window_size(500, 700) # about mobile size, portrait style
 
-  # TODO: those two calls just differ in the function to be called. Could be merged into a for loop?
-  if allTests[0] in testsToRun:
-    testNum = 1 # loginLogout
-    if (not(doLoginLogout(driver, testNum))):
-      finish(driver)
-    print("----")
-  # end if 
-
-  if allTests[1] in testsToRun:
-    testNum = 2 # newUser
-    if (not(doNewUser(driver, testNum))):
-      finish(driver)
-    print("----")
-  # end if 
-
-  if allTests[2] in testsToRun:
-    testNum = 3 # editLinks
-    if (not(doEditLinks(driver, testNum))):
-      finish(driver)
-    print("----")
-  # end if 
-
+  for i in range(0, len(ALL_TESTS)):
+    callSingleTest(driver, i, ALL_TESTS, testsToRun)
+  
   printOkOrNot(ok=True, testNum="==>", text="Selected tests execution")
   finish(driver)
 # end if len testsToRun
